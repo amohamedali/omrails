fb3ad4d9f86bed6b2b51d08f7dbfc0df7edbfe66
amohamedali <ahmed.mohamedali@epitech.eu>
2013-07-29 05:20:34 +0200

    PaperClip gem

--- db/migrate/20130729003839_create_pins.rb
+++ db/migrate/20130729003839_create_pins.rb
@@ -1,9 +1,9 @@
 class CreatePins < ActiveRecord::Migration
-  def change
+#  def change
 #    create_table :pins do |t|
-  #    t.string :description
+ #   t.string :description
 
- #     t.timestamps
-#    end
-  end
+  #   t.timestamps
+  # end
+  #end
 end
